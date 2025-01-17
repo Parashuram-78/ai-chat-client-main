4a01b699076a66d1127bc83fb765e74c87d3a6a2
Ankur-dev <itzankurhere@gmail.com>
2025-01-17 22:43:55 +0530

    binoloop changed

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+      },
+      {
+        protocol: "https",
+        hostname: "*",
+      },
+      {
+        protocol: "http",
+        hostname: "*",
+      },
+    ],
+  },
 };
 
-export default nextConfig;
+export default nextConfig;
